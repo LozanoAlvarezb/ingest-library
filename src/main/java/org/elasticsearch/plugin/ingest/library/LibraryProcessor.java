98604b7228fdf0c3f579d3dcd236210517fa2a83
bLozanoUPM <borja.lozano.alvarez@alumnos.upm.es.com>
2019-05-07 12:52:06 +0200

    Finally

--- src/main/java/org/elasticsearch/plugin/ingest/library/LibraryProcessor.java
+++ src/main/java/org/elasticsearch/plugin/ingest/library/LibraryProcessor.java
@@ -17,12 +17,14 @@
 
 package org.elasticsearch.plugin.ingest.library;
 
+import org.elasticsearch.cli.SuppressForbidden;
 import org.elasticsearch.ingest.AbstractProcessor;
 import org.elasticsearch.ingest.IngestDocument;
 import org.elasticsearch.ingest.Processor;
-import org.elasticsearch.SpecialPermission;
+//import org.elasticsearch.SpecialPermission;
 
 
+//import java.net.URLConnection;
 import java.util.Map;
 
 import java.io.BufferedReader;
@@ -56,42 +58,37 @@ public class LibraryProcessor extends AbstractProcessor {
     }
 
     @Override
+    @SuppressForbidden(reason = "Socket")
     public IngestDocument execute(IngestDocument ingestDocument) throws Exception {
 
         // Test URL
-        String url = " http://127.0.0.1:5000/user/Nicholas";
+        String url = "http://127.0.0.1:5000/user/Nicholas";
 
-
-
-        URL obj = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-//        con.setRequestMethod("GET");
-        if(con.getResponseCode()!=200){
-            return ingestDocument;
-        }
-
-        SecurityManager sm = System.getSecurityManager();
-        if (sm != null) {
-            // unprivileged code such as scripts do not have SpecialPermission
-            sm.checkPermission(new SpecialPermission());
-        }
         AccessController.doPrivileged((PrivilegedAction<Void>)
                 () -> {
                     try {
-                        InputStream is = con.getInputStream();
-                        BufferedReader in = new BufferedReader(new InputStreamReader(is,StandardCharsets.UTF_8));
-                        StringBuffer response = new StringBuffer();
-
-                        String inputLine;
-                        while ((inputLine = in.readLine()) != null) {
-                            response.append(inputLine);
+                        URL obj = new URL(url);
+                        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+//        con.setRequestMethod("GET");
+                        if(con.getResponseCode()!=200){
+                            return null;
                         }
-                        in.close();
+                        else{
+                            InputStream is = con.getInputStream();
+                            BufferedReader in = new BufferedReader(new InputStreamReader(is,StandardCharsets.UTF_8));
+                            StringBuilder response = new StringBuilder();
 
-                        //Read JSON response and print
-                        JSONObject myResponse = new JSONObject(response.toString());
-                        ingestDocument.setFieldValue(targetField, myResponse.getString("occupation"));
-                        return null;
+                            String inputLine;
+                            while ((inputLine = in.readLine()) != null) {
+                                response.append(inputLine);
+                            }
+                            in.close();
+
+                            //Read JSON response and print
+                            JSONObject myResponse = new JSONObject(response.toString());
+                            ingestDocument.setFieldValue(targetField, myResponse.getString("occupation"));
+                            return null;
+                        }
                     }catch(IOException ex){
                         return null;
                     }
@@ -101,6 +98,7 @@ public class LibraryProcessor extends AbstractProcessor {
 
         return ingestDocument;
     }
+
 
     @Override
     public String getType() {
